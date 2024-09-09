9728e5aa1bcd28e05eef8f382b39ae19ad6f99a5
davidwarner404 <testdome404@gmail.com>
2024-09-09 17:37:16 +0530

    updated files

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,27 @@
-# Use an official Python runtime as a parent image
-FROM python:3.9-slim
+# Use the official Python image from the Docker Hub
+FROM python:3.11-slim
+
+# Set environment variables
+ENV FLASK_APP=app.py
+ENV FLASK_ENV=production
 
 # Set the working directory in the container
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
-COPY . /app
+# Copy the requirements file into the container at /app
+COPY requirements.txt .
 
-# Install any needed packages specified in requirements.txt
+# Install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Make port 8080 available to the world outside this container
+# Copy the Firebase credentials into the container
+COPY e-template-manager-firebase-adminsdk-fn65x-75d0253fa3.json /app/
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Expose port 8080 for the Gunicorn server
 EXPOSE 8080
 
-# Define environment variable
-ENV FLASK_APP=app.py
-
-# Run the application
+# Command to run the application
 CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -1,156 +1,269 @@
+import io
 import json
-from flask import Flask, request, jsonify, redirect, url_for, render_template
-import os
-import imgkit
 import uuid
+from flask import Flask, request, jsonify, redirect, url_for, render_template, flash, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import imgkit
+from flask_wtf import CSRFProtect
+from flask_wtf.csrf import generate_csrf
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+csrf = CSRFProtect(app)
+@app.context_processor
+def csrf_context_processor():
+    return dict(csrf_token=generate_csrf())
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Update as needed
+load_dotenv()
+# Initialize Firebase Admin
+service_account_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY_PATH")
+cred = credentials.Certificate(service_account_path)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'e-template-manager.appspot.com'
+})
 
-TEMPLATES_FILE = 'templates.json'
+# Initialize Firestore
+db = firestore.client()
 
-def load_templates():
+# Initialize Cloud Storage
+bucket = storage.bucket()
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def get(user_id):
+        user_doc = db.collection('users').document(user_id).get()
+        if user_doc.exists:
+            user_data = user_doc.to_dict()
+            return User(user_id, user_data['email'], user_data['password'])
+        return None
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+def generate_thumbnail(template_html, new_template_id):
+    thumbnail_filename = f'thumbnail_{new_template_id}.jpg'
+    imgkit_options = {'format': 'jpg'}
+    
     try:
-        with open(TEMPLATES_FILE, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
+        img_data = imgkit.from_string(template_html, False, options=imgkit_options)
+        blob = bucket.blob(f'thumbnails/{thumbnail_filename}')
+        blob.upload_from_file(io.BytesIO(img_data), content_type='image/jpeg')
+        blob.make_public()
+        thumbnail_url = blob.public_url
+    except Exception as e:
+        print(f"Error generating or uploading thumbnail: {e}")
+        thumbnail_url = None
+    return thumbnail_url
 
-def save_templates(templates):
-    with open(TEMPLATES_FILE, 'w') as f:
-        json.dump(templates, f, indent=4)
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Login')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])  # Using StringField for email
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    submit = SubmitField('Sign Up') 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = db.collection('users').where('email', '==', email).get()
+        if user and check_password_hash(user[0].to_dict()['password'], password):
+            user_id = user[0].id
+            login_user(User(user_id, email, user[0].to_dict()['password']))
+            flash('Login successful.', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your email and/or password.', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        if password == form.confirm_password.data:
+            hashed_password = generate_password_hash(password, method='sha256')
+            user_ref = db.collection('users').add({
+                'username':username,
+                'email': email,
+                'password': hashed_password
+            })
+            flash('Account created successfully. You can now log in.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Passwords do not match.', 'danger')
+    return render_template('signup.html', form=form)
 
 @app.route('/upload', methods=['POST'])
+@login_required
 def upload_template():
-    templates = load_templates()
-    new_template_id = str(uuid.uuid4())  # Generate a unique UUID
+    new_template_id = str(uuid.uuid4())
     new_template = {
-        "id": new_template_id,
         "subject_line": request.form['subject_line'],
         "segment": request.form['segment'],
         "template": request.form['template'],
-        "date": request.form['date']
+        "date": request.form['date'],
+        "added_by": current_user.id
     }
     
-    # Generate a thumbnail image
-    thumbnail_filename = f'thumbnail_{new_template_id}.jpg'
-    thumbnail_path = os.path.join('static', 'thumbnails', thumbnail_filename)
+    thumbnail_url = generate_thumbnail(new_template['template'], new_template_id)
+    new_template['thumbnail'] = thumbnail_url
+    db.collection('templates').add(new_template)
 
-    # Create a temporary HTML file to render to image
-    temp_html_path = 'temp_template.html'
-    with open(temp_html_path, 'w', encoding="utf-8") as temp_html_file:
-        temp_html_file.write(new_template['template'])
-
-    imgkit.from_file(temp_html_path, thumbnail_path)
-    os.remove(temp_html_path)  # Clean up temporary file
-    new_template['thumbnail'] = thumbnail_filename
-
-    templates.append(new_template)
-    save_templates(templates)
-
-    return redirect(url_for('index'))
+    return jsonify({'success': True})
 
 @app.route('/update/<string:template_id>', methods=['POST'])
+@login_required
 def update_template(template_id):
-    templates = load_templates()
-    template = next((template for template in templates if template["id"] == template_id), None)
-   
-    if template is None:
+    template_ref = db.collection('templates').document(template_id)
+    template = template_ref.get().to_dict()
+
+    if not template:
         return jsonify({'error': 'Template not found'}), 404
     
-    # Remove the old thumbnail if it exists
-    thumbnail_path = os.path.join('static', 'thumbnails', template['thumbnail'])
-    if os.path.exists(thumbnail_path):
-        os.remove(thumbnail_path)
-    
-    thumbnail_filename = f'thumbnail_{template_id}.jpg'
-    thumbnail_path = os.path.join('static', 'thumbnails', thumbnail_filename)
-    
-    # Create a temporary HTML file to render to image
-    temp_html_path = 'temp_template.html'
-    with open(temp_html_path, 'w', encoding="utf-8") as temp_html_file:
-        temp_html_file.write('<!DOCTYPE html><html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type" /><meta content="width=device-width, initial-scale=1.0" name="viewport" /><title></title></head><body style="margin: 0; padding: 1%; background-color: #f3f4f6; font-family: Arial, sans-serif; text-align: center;">' + template['template'] + '</body></html>')
+    old_thumbnail_url = template.get('thumbnail', '')
+    if old_thumbnail_url:
+        old_thumbnail_filename = old_thumbnail_url.split('/')[-1]
+        old_blob = bucket.blob(f'thumbnails/{old_thumbnail_filename}')
+        old_blob.delete()
 
-    imgkit.from_file(temp_html_path, thumbnail_path)
-    os.remove(temp_html_path)  # Clean up temporary file
-    
-    # Update the template with the new data
-    template['subject_line'] = request.form['subject_line']
-    template['segment'] = request.form['segment']
-    template['template'] = request.form['template']
-    template['date'] = request.form['date']
-    template['thumbnail'] = thumbnail_filename
+    thumbnail_url = generate_thumbnail(request.form['template'], template_id)
+    updated_template = {
+        'subject_line': request.form['subject_line'],
+        'segment': request.form['segment'],
+        'template': request.form['template'],
+        'date': request.form['date'],
+        'thumbnail': thumbnail_url
+    }
 
-    # Save the updated templates
-    save_templates(templates)
+    template_ref.update(updated_template)
 
     return redirect(url_for('index'))
 
 @app.route('/template/<string:template_id>', methods=['GET'])
 def get_template(template_id):
-    templates = load_templates()
-    template = next((template for template in templates if template["id"] == template_id), None)
+    template_ref = db.collection('templates').document(template_id)
+    template = template_ref.get().to_dict()
 
-    if template is None:
+    if not template:
         return jsonify({'error': 'Template not found'}), 404
 
     return jsonify({'template': template['template']})
 
 @app.route('/template/<string:template_id>', methods=['DELETE'])
+@login_required
 def delete_template(template_id):
-    templates = load_templates()
-    template_to_delete = next((template for template in templates if template["id"] == template_id), None)
-    
-    if not template_to_delete:
+    template_ref = db.collection('templates').document(template_id)
+    template = template_ref.get().to_dict()
+
+    if not template:
         return jsonify({'error': 'Template not found'}), 404
 
     try:
-        thumbnail_path = os.path.join('static', 'thumbnails', template_to_delete['thumbnail'])
-        if os.path.exists(thumbnail_path):
-            os.remove(thumbnail_path)
-        templates = [template for template in templates if template["id"] != template_id]
-        
-        save_templates(templates)
+        thumbnail_path = template['thumbnail'].split('/')[-1]
+        blob = bucket.blob(f'thumbnails/{thumbnail_path}')
+        blob.delete()
+
+        template_ref.delete()
+
         return '', 204
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 @app.route('/edit/<string:template_id>', methods=['GET'])
+@login_required
 def edit_template(template_id):
-    templates = load_templates()
-    template = next((template for template in templates if template["id"] == template_id), None)
+    template_ref = db.collection('templates').document(template_id)
+    template = template_ref.get().to_dict()
+    template['id'] = template_id
 
-    if template is None:
+    if not template:
         return jsonify({'error': 'Template not found'}), 404
 
     return render_template('edit.html', template=template)
 
-@app.route('/edit/new', methods=['GET'])
-def new_template():
-    new_template_id = str(uuid.uuid4())  # Generate a unique UUID
-    new_template = {
-        "id": new_template_id,
-        "subject_line": "",
-        "segment": "opener",  # Default segment
-        "template": "Add Your Creative Here",
-        "thumbnail": "thumbnail_new.jpg",
-        "date": ""  # Default date
-    }
-
-    return render_template('edit.html', template=new_template)
-
-@app.route('/', methods=['GET'])
+@app.route('/')
+@login_required
 def index():
     templates = load_templates()
+    users = load_users()
     date_filter = request.args.get('date')
     segment_filter = request.args.get('segment')
 
-    if date_filter:
+    if date_filter and segment_filter:
+        # Apply both filters
+        templates = [template for template in templates if template['date'] == date_filter and template['segment'] == segment_filter]
+    elif date_filter:
+        # Apply only date filter
         templates = [template for template in templates if template['date'] == date_filter]
-
-    if segment_filter:
+    elif segment_filter:
+        # Apply only segment filter
         templates = [template for template in templates if template['segment'] == segment_filter]
 
-    return render_template('index.html', templates=templates)
+    csrf_token = generate_csrf()
+
+    return render_template('index.html', templates=templates, users=users, csrf_token=csrf_token)
+
+def get_username(user_id):
+    user_doc = db.collection('users').document(user_id).get()
+    if user_doc.exists:
+        return user_doc.to_dict().get('username', '')
+    return ''
+
+def load_users():
+    users=[]
+    docs=db.collection('users').stream()
+    for doc in docs:
+        user= doc.to_dict()
+        user['id'] = doc.id
+        users.append(user)
+    return users
+
+
+def load_templates():
+    templates = []
+    docs = db.collection('templates').stream()
+    for doc in docs:
+        template = doc.to_dict()
+        template['id'] = doc.id
+        templates.append(template)
+    return templates
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

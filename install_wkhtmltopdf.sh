ebfd7766d014579819c0e52893884bc8da1dc513
davidwarner404 <testdome404@gmail.com>
2024-09-10 10:37:14 +0530

    wkhtmltopdf.sh problem

--- install_wkhtmltopdf.sh
+++ install_wkhtmltopdf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update package list
-sudo apt-get update
+apt-get update
 
 # Install wkhtmltopdf (this includes wkhtmltoimage)
-sudo apt-get install -y wkhtmltopdf
+apt-get install -y wkhtmltopdf
 
 # Optional: Verify installation
 wkhtmltoimage --version
